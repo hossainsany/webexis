93046c9645fb4072317fa039088f20a10085e3d4
Hossain Sany <hossainsany3@gmail.com>
2023-10-07 01:04:43 +0600

    redirect

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,7 @@
 import About from '@components/About';
 import Hero from '@components/Hero';
-import { redirect } from 'next/navigation';
 
 const Home = () => {
-    redirect('/coming-soon');
-
     return (
         <>
             <Hero />
