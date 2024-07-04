c382c9f2ab51c91cb5b762a6abf1b0cb757875c3
Hossain Sany <hossainsany3@gmail.com>
2024-07-04 14:09:29 +0600

    seo optimizations

--- app/layout.js
+++ app/layout.js
@@ -10,9 +10,33 @@ const poppins = Poppins({
 });
 
 export const metadata = {
-    title: 'Webexis - Grow Your Service Business with Powerful Web Design',
+    metadataBase: new URL('https://webexis.net'),
+    title: {
+        default: 'WebExis - Your Web Design, Development, and Marketing Partner',
+        template: '%s | WebExis',
+    },
+    keywords: [
+        'webexis',
+        'web design',
+        'Website development',
+        'digital marketing agency',
+        'Custom web solutions',
+        'best web design agency',
+        'best web design agency in sylhet',
+        'SEO (Search Engine Optimization)',
+        'Social media marketing',
+        'Affordable web design services for small businesses',
+        'Custom website development solutions for startups',
+        'Local SEO services for regional businesses',
+        'Social media marketing campaigns for increased engagement',
+    ],
     description:
-        'Attract more clients, showcase your expertise, and simplify lead capture with Webexis. We design beautiful, user-friendly websites for service businesses to boost online presence, generate leads, and grow your business.',
+        'Elevate your online presence with WebExis. We specialize in custom web design, web development, and digital marketing strategies. Our expert team creates stunning, user-friendly websites to attract clients, streamline lead generation, and propel your business forward.',
+    openGraph: {
+        description:
+            'Elevate your online presence with WebExis. We specialize in custom web design, web development, and digital marketing strategies. Our expert team creates stunning, user-friendly websites to attract clients, streamline lead generation, and propel your business forward.',
+        images: [''],
+    },
 };
 
 export default function RootLayout({ children }) {
---
+++ app/sitemap.js
@@ -0,0 +1,32 @@
+export default async function sitemap() {
+    return [
+        {
+            url: 'https://webexis.net',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://webexis.net/about',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://webexis.net/services',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://webexis.net/contact',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://webexis.net/tos',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://webexis.net/cookie-policy',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://webexis.net/refund-policy',
+            lastModified: new Date(),
+        },
+    ];
+}
