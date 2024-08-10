6fc88cf35d9fcc321d6fd17bce4d51570239e25b
Hossain Sany <hossainsany3@gmail.com>
2024-08-10 07:35:05 +0600

    updated sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -5,7 +5,7 @@ export default async function sitemap() {
             lastModified: new Date(),
         },
         {
-            url: 'https://webexis.net/about',
+            url: 'https://webexis.net/services',
             lastModified: new Date(),
         },
         {
@@ -13,7 +13,19 @@ export default async function sitemap() {
             lastModified: new Date(),
         },
         {
-            url: 'https://webexis.net/services',
+            url: 'https://webexis.net/case-studies/mosaic-plugin-landing-page-case-study',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://webexis.net/case-studies/sampan-restaurant-website-case-study',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://webexis.net/case-studies/webexis-website-case-study',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://webexis.net/case-studies/frontend-youtube-case-study',
             lastModified: new Date(),
         },
         {
@@ -30,6 +42,26 @@ export default async function sitemap() {
         },
         {
             url: 'https://webexis.net/refund-policy',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://webexis.net/privacy-policy',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://www.linkedin.com/company/webexis',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://www.instagram.com/webexis_/',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://www.facebook.com/webexis.net/',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://twitter.com/WebExis_',
             lastModified: new Date(),
         },
     ];
